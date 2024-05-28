850d9b6843a8a7895321c1f889f3e5dd4e14e702
Sam Zaccone <samrzaccone@gmail.com>
2024-05-27 23:16:04 -0400

    sync firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+import 'utils/firebase_utils/firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.web);
   runApp(const MyApp());
 }
 
